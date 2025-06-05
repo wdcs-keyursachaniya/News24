79890f61a9afd931701b943a835ceb95c689ff68
wdcs-keyursachaniya <keyur.sachaniya@webcluesinfotech.com>
2025-06-05 13:39:31 +0530

    Version upgrade

---
+++ pages/studio/layout.tsx
@@ -0,0 +1,13 @@
+import {preloadModule} from 'react-dom'
+
+const bridgeScript = 'https://core.sanity-cdn.com/bridge.js'
+
+export default function StudioLayout({ children }: { children: React.ReactNode }) {
+  preloadModule(bridgeScript, {as: 'script'})
+  return (
+    <>
+      <script src={bridgeScript} async type="module" />
+      {children}
+    </>
+  )
+}
