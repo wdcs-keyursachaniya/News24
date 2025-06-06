12725dcf0d3c52fb3b0d4180062807207bceb28c
wdcs-keyursachaniya <keyur.sachaniya@webcluesinfotech.com>
2025-06-06 12:49:16 +0530

    add manifest files and change base path

--- sanity.config.ts
+++ sanity.config.ts
@@ -136,7 +136,7 @@ const sharedConfig = definePlugin({
 export default defineConfig([
   {
     name: 'staging',
-    basePath: '/studio/staging',
+    basePath: '/',
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset: 'staging',
     title: 'Staging',
@@ -144,7 +144,7 @@ export default defineConfig([
   },
   {
     name: 'Production',
-    basePath: '/studio/production',
+    basePath: '/',
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset: 'production',
     title: 'Production',
